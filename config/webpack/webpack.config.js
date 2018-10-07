577b9ec5c761e2b2b34c77969399ba39aa29a1ba
Scott Silverstein <scsilver.umd@gmail.com>
2018-10-06 23:34:30 -0600

    build

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -4,22 +4,22 @@ const webpack = require("webpack");
 
 module.exports = {
     module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["env", "react"],
-            plugins: [
-              "transform-decorators-legacy",
-              "transform-class-properties",
-              "syntax-object-rest-spread"
-            ]
+      rules: [
+        {
+          test: /\.(js|jsx|ts)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["env", "react"],
+              plugins: [
+                 "transform-decorators-legacy",
+                "transform-class-properties",
+                "syntax-object-rest-spread"
+              ]
+            }
           }
         }
-      }
-    ]
-  }
+      ] 
+    }
 };
